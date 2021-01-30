2b40e2e9e1621e3d37fb370fb33fe1c2bbd8058b
ExselcisDeo <69751569+Einsviel@users.noreply.github.com>
2021-01-30 12:32:22 +0700

    Homepage done
    kontak jika tidak sesuai

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,9 @@
-(function(){
-    $('#msbo').on('click', function(){
-      $('body').toggleClass('msb-x');
-    });
-  }());
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
